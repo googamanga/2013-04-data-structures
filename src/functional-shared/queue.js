16c0a6c0a47cc4ff49f84564a8f762580b46b7e9
Hack Reactor Students <github@hackreactor.com>
2013-04-29 16:21:28 -0700

    Functional-shared queue passing all tests

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,1 +1,40 @@
-// Hey! Copy your code from src/functional/queue.js and paste it here
+var extend = function(obj, extention) {
+  var keys = Object.keys(extention);
+
+  for(var i =0; i < keys.length; i++){
+    var key = keys[i];
+    obj[key] = extention[key];
+  }
+  return obj;
+};
+
+
+var makeQueue = function(){
+  // Use an object with numeric keys to store values
+  //var storage = {};
+  var queue = {};
+  queue.startIndex = 0;
+  queue.endIndex = 0;
+  queue.storage = {};
+  extend(queue, queueMethods);
+  return queue;
+};
+
+queueMethods= {};
+
+queueMethods.enqueue = function(value){
+  this.startIndex -= 1;
+  return this.storage[this.startIndex] = value;
+};
+
+queueMethods.dequeue = function(){
+  if(this.size() <= 0 ){
+    throw "Queue is size 0, can't dequeue";
+  }
+  this.endIndex -= 1;
+  return this.storage[this.endIndex];
+};
+
+queueMethods.size = function(){
+  return this.endIndex - this.startIndex;
+};
