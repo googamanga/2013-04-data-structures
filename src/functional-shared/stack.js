58ed5726c1e96d9c8f9ec20e6a1f6814b332d00c
Hack Reactor Students <github@hackreactor.com>
2013-04-29 14:50:19 -0700

    Clearing functional methods copied from shared

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -8,38 +8,27 @@ var extend = function(obj, extention) {
   return obj;
 };
 
+
 var makeStack = function(){
   // Use an object with numeric keys to store values
   //var storage = {};
 
-
   // Implement the methods below
-  var stack = {
-    size: 0,  // Hint: set an initial value here
-    storage: {}
-  };
-  extend(stack, stackMethods);
+  var stack = {};
+
   return stack;
 };
 
 stackMethods= {};
 
 stackMethods.add = function(value){
-  this.storage[this.size] = value;
-  this.size += 1;
-  return value;
+
 };
 
 stackMethods.remove = function(){
-  if(this.size <= 0){
-    throw "Stack is empty";
-  }
-  this.size -= 1;
-  var value = this.storage[this.size];
-  delete this.storage[this.size];
-  return value;
+
 };
 
 stackMethods.size = function(){
-  return this.size;
+
 };
