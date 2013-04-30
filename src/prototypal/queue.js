faa29afa63636617d3c0d584d3804397fcb74319
Hack Reactor Students <github@hackreactor.com>
2013-04-29 17:18:54 -0700

    Prototypal queue and stack pass tests

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,1 +1,37 @@
-// Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var extend = function(obj, extention) {
+  var keys = Object.keys(extention);
+
+  for(var i =0; i < keys.length; i++){
+    var key = keys[i];
+    obj[key] = extention[key];
+  }
+  return obj;
+};
+
+
+var Queue = function(){
+  // Use an object with numeric keys to store values
+  //var storage = {};
+  var queue = Object.create(Queue.prototype);
+  queue.startIndex = 0;
+  queue.endIndex = 0;
+  queue.storage = {};
+  return queue;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.startIndex -= 1;
+  return this.storage[this.startIndex] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.size() <= 0 ){
+    throw "Queue is size 0, can't dequeue";
+  }
+  this.endIndex -= 1;
+  return this.storage[this.endIndex];
+};
+
+Queue.prototype.size = function(){
+  return this.endIndex - this.startIndex;
+};
--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,1 +1,39 @@
-// Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var extend = function(obj, extention) {
+  var keys = Object.keys(extention);
+
+  for(var i =0; i < keys.length; i++){
+    var key = keys[i];
+    obj[key] = extention[key];
+  }
+  return obj;
+};
+
+
+var Stack = function(){
+  // Use an object with numeric keys to store values
+  //var storage = {};
+  var stack = Object.create(Stack.prototype);
+  stack.index = 0;
+  stack.storage = {};
+  return stack;
+};
+
+Stack.prototype.add = function(value){
+  this.storage[this.index] = value;
+  this.index += 1;
+  return value;
+};
+
+Stack.prototype.remove = function(){
+  if(this.size() <= 0){
+    throw "Stack is empty";
+  }
+  this.index -= 1;
+  var value = this.storage[this.index];
+  delete this.storage[this.index];
+  return value;
+};
+
+Stack.prototype.size = function(){
+  return this.index;
+};
